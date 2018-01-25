dddbd3a8e5dd7fee1f98e41f48990854c11c5e8f
bondy lee <bondy.lee@outlook.com>
2018-01-24 22:13:32 -0500

    make nav bar works good in mobile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -10,13 +11,14 @@ import { AboutComponent } from './about/about.component';
 import { PersonserviceComponent } from './personservice/personservice.component';
 import { BusinessserviceComponent } from './businessservice/businessservice.component';
 import { ContactComponent } from './contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', redirectTo: 'about', pathMatch: 'full' },
   { path: 'about', component: AboutComponent },
   { path: 'personservice', component: PersonserviceComponent },
   { path: 'businessservice', component: BusinessserviceComponent },
-  { path: 'contact',      component: ContactComponent }
+  { path: 'contact', component: ContactComponent }
 ];
 
 @NgModule({
@@ -26,12 +28,14 @@ const appRoutes: Routes = [
     AboutComponent,
     PersonserviceComponent,
     BusinessserviceComponent,
-    ContactComponent
+    ContactComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    NgbModule.forRoot(),
     RouterModule.forRoot(appRoutes, { enableTracing: true }) // <-- debugging purposes only
   ],
   providers: [],
